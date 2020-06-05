f6a41f66eeed2ee1df12b2f0817de92c8dd0e7e1
Said <s.lotus69@gmail.com>
2020-06-05 23:59:55 +0200

    first commit

---
+++ models.py
@@ -0,0 +1,80 @@
+import os
+from sqlalchemy import Column, String, Integer, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+database_name = "messagor"
+database_path = "postgres://{}:{}@{}/{}".format(
+    'postgres', 'password', 'localhost:5432', database_name)
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+'''
+Room
+
+'''
+
+
+class Room(db.Model):
+    __tablename__ = 'rooms'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    messages = relationship("Message", backref='room', lazy = True, cascade="all, delete-orphan")
+
+    def __init__(self, name):
+        self.name = name
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
+
+
+'''
+Message
+
+'''
+
+
+class Message(db.Model):
+    __tablename__ = 'messages'
+
+    id = Column(Integer, primary_key=True)
+    content = Column(String)
+    room_id = db.Column(db.Integer, db.ForeignKey('rooms.id'), nullable=False)
+
+    def __init__(self, content):
+        self.content = content
+
+    def format(self):
+        return {
+            'id': self.id,
+            'content': self.content
+        }
