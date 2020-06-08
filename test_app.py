53bfd1afddcadba737ee78a5e06ecb919dc8f95f
Said <s.lotus69@gmail.com>
2020-06-08 14:34:04 +0200

    feat setup testcase and auth

---
+++ test_app.py
@@ -0,0 +1,31 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
+
+from app import create_app
+from models import setup_db, Room, Message
+
+
+class MessagorTestCase(unittest.TestCase):
+    """This class represents the trivia test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "messagor_test"
+        self.database_path = "postgres://{}:{}@{}/{}".format(
+            'postgres', 'password', 'localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
+
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
+
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
